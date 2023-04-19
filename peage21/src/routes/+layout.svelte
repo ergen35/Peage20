e03f0b34a3d47ae414c35a7a139de0e19f4edf9c
averymkv3 <ienoverse@gmail.com>
2023-04-19 17:12:30 +0100

    initial commit

---
+++ peage21/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import "../app.css";
+    import type { LayoutData } from './$types';
+    
+    export let data: LayoutData;
+</script>
+
+
+
+
+<slot>
+
+</slot>
---
+++ peage21/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { Alert, Button } from 'flowbite-svelte'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="p-8">
+    <Alert>
+        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </Alert>
+
+    <p class="mt-5">
+        <Button color="purple">A new button</Button>
+    </p>
+</div>
